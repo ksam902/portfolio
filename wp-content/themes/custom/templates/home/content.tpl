94accbca02cded55d3fe9d5d8c65c8920504cb01
Kyle Samson <kylesamson21@gmail.com>
2015-03-10 21:32:40 -0300

    sample hover effects

--- wp-content/themes/custom/templates/home/content.tpl
+++ wp-content/themes/custom/templates/home/content.tpl
@@ -1,1 +1,20 @@
 <div>HEY THERE GOOD LOOKING </div>
+                <h2>Portfolio Samples</h2>
+                <div class="grid">
+                    <figure class="effect-oscar">
+                        <img src="img/9.jpg" alt="img09"/>
+                        <figcaption>
+                            <h2>Sample <span>1</span></h2>
+                            <p>Sample 1 Content</p>
+                            <a href="#">View more</a>
+                        </figcaption>
+                    </figure>
+                    <figure class="effect-oscar">
+                        <img src="img/10.jpg" alt="img10"/>
+                        <figcaption>
+                            <h2>Sample <span>2</span></h2>
+                            <p>Sample 2 Content</p>
+                            <a href="#">View more</a>
+                        </figcaption>
+                    </figure>
+                </div>
--- wp-content/themes/custom/templates_c/151b3b0105cb4d4dc0d26b1e3f4d1473e73ebe11.file.content.tpl.php
+++ wp-content/themes/custom/templates_c/151b3b0105cb4d4dc0d26b1e3f4d1473e73ebe11.file.content.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.18, created on 2015-03-10 23:58:24
+<?php /* Smarty version Smarty-3.1.18, created on 2015-03-11 00:31:51
          compiled from "/Applications/XAMPP/xamppfiles/htdocs/portfolio/wp-content/themes/custom/templates/home/content.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:125287667754ff7aea8a2221-84197303%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '151b3b0105cb4d4dc0d26b1e3f4d1473e73ebe11' => 
     array (
       0 => '/Applications/XAMPP/xamppfiles/htdocs/portfolio/wp-content/themes/custom/templates/home/content.tpl',
-      1 => 1426031887,
+      1 => 1426033906,
       2 => 'file',
     ),
   ),
@@ -20,4 +20,22 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_54ff7aea8a3ab5_42620581')) {function content_54ff7aea8a3ab5_42620581($_smarty_tpl) {?><div>HEY THERE GOOD LOOKING </div>
-<?php }} ?>
+                <h2>Oscar</h2>
+                <div class="grid">
+                    <figure class="effect-oscar">
+                        <img src="img/9.jpg" alt="img09"/>
+                        <figcaption>
+                            <h2>Sample <span>1</span></h2>
+                            <p>Sample 1 Content</p>
+                            <a href="#">View more</a>
+                        </figcaption>
+                    </figure>
+                    <figure class="effect-oscar">
+                        <img src="img/10.jpg" alt="img10"/>
+                        <figcaption>
+                            <h2>Sample <span>2</span></h2>
+                            <p>Sample 2 Content</p>
+                            <a href="#">View more</a>
+                        </figcaption>
+                    </figure>
+                </div><?php }} ?>
